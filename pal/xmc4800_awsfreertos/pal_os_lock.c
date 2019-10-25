419f367444af4339fd3e9cd15e5c1388a3a2db3f
Artem Yushev <12692378+ayushev@users.noreply.github.com>
2019-10-25 12:16:42 +0200

    Update pal_os_lock.c

--- pal/xmc4800_awsfreertos/pal_os_lock.c
+++ pal/xmc4800_awsfreertos/pal_os_lock.c
@@ -1,7 +1,7 @@
 /**
 * MIT License
 *
-* Copyright (c) 2018 Infineon Technologies AG
+* Copyright (c) 2019 Infineon Technologies AG
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -32,38 +32,36 @@
 
 #include "optiga/pal/pal_os_lock.h"
 
-/**
- * @brief PAL OS lock structure. Might be extended if needed
- */
-typedef struct pal_os_lock
-{
-    uint8_t lock;
-} pal_os_lock_t;
+#include "FreeRTOS.h"
+#include "semphr.h"
 
-volatile static pal_os_lock_t pal_os_lock = {.lock = 0};
+SemaphoreHandle_t xLockSemaphoreHandle;
+
+volatile uint8_t first_call_flag = 1;
+
+void _lock_init(void)
+{
+	xLockSemaphoreHandle = xSemaphoreCreateBinary();
+	pal_os_lock_release();
+}
 
 pal_status_t pal_os_lock_acquire(void)
 {
-    pal_status_t return_status = PAL_STATUS_FAILURE;
+	vPortEnterCritical();
+	if (first_call_flag)
+	{
+		_lock_init();
+		first_call_flag = 0;
+	}
+	vPortExitCritical();
 
-    if(!(pal_os_lock.lock))
-    {
-    	pal_os_lock.lock++;
-        if(pal_os_lock.lock != 1)
-        {
-        	pal_os_lock.lock--;
-        }
-        return_status = PAL_STATUS_SUCCESS;
-    }
-    return return_status;
+	if ( xSemaphoreTake(xLockSemaphoreHandle, portMAX_DELAY) == pdTRUE )
+		return PAL_STATUS_SUCCESS;
 }
 
 void pal_os_lock_release(void)
 {
-    if(pal_os_lock.lock)
-    {
-    	pal_os_lock.lock--;
-    }
+	xSemaphoreGive(xLockSemaphoreHandle);
 }
 
 /**
