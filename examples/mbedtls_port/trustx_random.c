a610c5ad2f91df31ca5eb9413cd3d122518d688e
Artem Yushev <12692378+ayushev@users.noreply.github.com>
2019-11-27 13:41:29 +0100

    Update trustx_random.c

--- examples/mbedtls_port/trustx_random.c
+++ examples/mbedtls_port/trustx_random.c
@@ -1,32 +1,3 @@
-/**
-* MIT License
-*
-* Copyright (c) 2018 Infineon Technologies AG
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE
-*
-* @{
-*/
-
-#include "optiga/optiga_crypt.h"
-#include "optiga/optiga_util.h"
-
 #if !defined(MBEDTLS_CONFIG_FILE)
 #include "mbedtls/config.h"
 #else
@@ -36,21 +7,26 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "optiga/optiga_crypt.h"
+#include "mbedtls/entropy_poll.h"
 
 #if defined(MBEDTLS_ENTROPY_HARDWARE_ALT)
+
 
 int mbedtls_hardware_poll( void *data,
                            unsigned char *output, size_t len, size_t *olen )
 {
-    optiga_lib_status_t status;
-    
-    if (len <= *olen) 
-    {
-        status = optiga_crypt_random(OPTIGA_RNG_TYPE_TRNG, output, len);
-        if (status == OPTIGA_LIB_SUCCESS )
-            *olen = len;
-    }
 
-    return 0;
+	optiga_lib_status_t status = OPTIGA_LIB_ERROR;
+
+	status = optiga_crypt_random(eTRNG, output, len);
+	if ( status !=  OPTIGA_LIB_SUCCESS)
+	{
+		*olen = 0;
+		return 1;
+	}
+
+	*olen = len;
+	return 0;
 }
 #endif
